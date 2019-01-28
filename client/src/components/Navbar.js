d076bb3bb463cc681e1b492a63ede6fa67c6b549
Mopztopz <stephenbrown83@gmail.com>
2019-01-28 11:47:00 -0600

    Update Navbar link names in  Navbar.js component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,20 +11,17 @@ class Navbar extends Component {
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Video</a>
+                </li>
+            </ul>
                 </div>
             </nav>
         );
