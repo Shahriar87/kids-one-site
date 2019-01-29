137aba7c5a27af7e3c6d7471671bfdb4f3ee581a
Mopztopz <stephenbrown83@gmail.com>
2019-01-28 22:29:32 -0600

    added iframe for GameBody.js without the  URL

--- client/src/components/GameBody.js
+++ client/src/components/GameBody.js
@@ -1,12 +1,20 @@
 // -----GameBody component
 import React, { Component } from "react";
 
+
 class GameBody extends Component {
     render() {
         return (
             <div>
-                <h1>The Game Page!</h1>
-            </div>
+            <h1>Hello, world!</h1>
+                <div class="row">
+                    <div class="col-md-2">instructions</div>
+                    <div class=" col-md-8 embed-responsive embed-responsive-16by9">
+                     <iframe class="embed-responsive-item" src=""></iframe>
+                    </div>                                        
+                    <div class="col-md-2">TBD</div>
+                </div>
+              </div>
         );
     }
 }
