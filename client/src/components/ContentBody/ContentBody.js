9a8d1a2fdd741a898960b963628b1082d946ad54
Innovator <shahriar.firoz@gmail.com>
2019-01-29 21:42:25 -0600

    Designs added

--- client/src/components/ContentBody/ContentBody.js
+++ client/src/components/ContentBody/ContentBody.js
@@ -2,26 +2,39 @@
 import React, { Component } from "react";
 import App from "../messenger/App";
 
+const styles = {
+    width: "18rem"
+};
+
 class ContentBody extends Component {
     render() {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <div class="row cont">
-                    <div class="col_1 col-sm-3 card_body">News</div>
-                    <div class="card">
-                        <div class="card-header">
+                <div className="row cont">
+                    <div className="col_1 col-sm-3 card_body">News</div>
+                    <div className="card">
+                        <div className="card-header">
                             Featured
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
-                    <div class="col_2 col-sm-6">Main</div>
-                    <div class="col_3 col-sm-3">
-                        <App />
+                    <div className="col_2 col-sm-6">Main</div>
+
+
+                    <div className="col_3 col-sm-3">
+                    <div className="card" style={styles}>
+                            <div className="card-header">
+                                Messages
+                </div>
+                <div className="card-body">
+                            <App />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
